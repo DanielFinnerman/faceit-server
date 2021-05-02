4d16b54c08d931da3ef44a65e7763f096155d4ce
Daniel Finnerman <danu95d@hotmail.com>
2021-05-03 00:25:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# FaceitProject
+
+React application with GraphQL API integration using Express, Mongoose (MongoDB Atlas), Apollo Server and React Semantic UI.
+Uses Faceit API to display stats of faceit.com users.
+
+Create blog posts, comment and like them. Display stats from faceit.com users
---
+++ index.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+const port = process.env.port || 3001;
+
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => ({req})
+});
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,  useUnifiedTopology: true})
+	.then(() => {
+		console.log('MongoDB connected');
+		return server.listen(port)
+})
+	.then(res => {
+		console.log(`Server running at ${res.url}`);
+	})
+	.catch(err => {
+		console.log(err);
+	})
